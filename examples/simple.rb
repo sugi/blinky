b921661abbf4cba33918f0a34e48faa15834725f
Tatsuki Sugiura <sugi@nemui.org>
2016-11-17 22:06:20 +0900

    Update sample script.

--- examples/simple.rb
+++ examples/simple.rb
@@ -10,7 +10,8 @@ WebShot.configure do |c|
 end
 
 ren = WebShot::Renderer.new
-req = WebShot::Request.new uri: 'http://ruby-lang.org/', imgsize: [400, 400], winsize: [1200, 1600]
+req = WebShot::Request.new uri: ARGV.first || 'http://ruby-lang.org/', imgsize: [400, 400], winsize: [1200, 1600]
 File.open('screenshot.png', 'w') do |f|
   f << ren.render(req)
 end
+puts "Screenshot has been saved to screenshot.png"
